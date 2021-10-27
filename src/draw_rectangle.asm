239ffcd441ae9909117991e1d9e6a6ac42bd94bf
Tina <44570841+tina-hoeflich@users.noreply.github.com>
2021-10-27 10:53:48 +0200

    Update draw_rectangle.asm
    Changes only in code documentation

--- src/draw_rectangle.asm
+++ src/draw_rectangle.asm
@@ -1,25 +1,21 @@
 # Based on CESP lecture
-# Authors: Miriam Penger, Lena Gerken, Tina Höflich
-
-
+# Authors: Miriam Penger, Lena Gerken, Tina HÃ¶flich
 
 # Function to draw rectangle from position x1,y1 to x2,y2 with fill color c
+draw_rectangle:
+#----Inputs--------
 # a3: unsigned integer x1 -- left boundary of rectangle
 # a4: unsigned integer y1 -- top boundary of rectangle
 # a5: unsigned integer x2 -- right boundary of rectangle
 # a6: unsigned integer y2 -- bottom boundary of rectangle
 # a7: unsigned integer c  -- fill color of rectangle as RGB value
 
-draw_rectangle:
-	#Save all necessary values to the stack
-
-
+	#memory allocation
 	addi sp, sp, -36
 	sw ra, (sp)
 	sw s0, 4 (sp)
 	sw s1, 8 (sp)
 	sw s2, 12 (sp)
-	# a3-a7 are stored as local variable on the stack
 	sw a3, 16 (sp)
 	sw a4, 20 (sp)
 	sw a5, 24 (sp)
@@ -32,16 +28,15 @@ draw_rectangle:
 	
 	_loopy:
 		_loopx:
+		 	#Moving values to registers a1-a3 to call draw_pixel
 			mv a1, t0
 			mv a2, t1
 			mv a3, a7
-			#STEP x: Move the right value to registers a1-a3 and call draw_pixel
-			# ADD YOUR STEP x CODE HERE
 			jal draw_pixel
 			
-			addi t0, t0, 1
+			addi t0, t0, 1 #x=x+1
 			bne t0, a5, _loopx
-	addi t1, t1, 1
+	addi t1, t1, 1 #y +=1 
 	lw t0, 16 (sp)
 	bne t1, a6, _loopy
 
@@ -52,7 +47,3 @@ draw_rectangle:
 	# a3-a7 do not need to be restored according to calling convention
 	addi sp, sp, 36
 	ret
-	# ADD YOUR STEP x CODE HERE
-
-.include "draw_pixel.asm"
-
