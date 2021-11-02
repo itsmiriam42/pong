efb06ae6c6661bd1e634c083e16d9133f13801bc
lena143 <80892941+lena143@users.noreply.github.com>
2021-11-02 20:23:44 +0100

    added a control function

--- src/ball_actions.asm
+++ src/ball_actions.asm
@@ -1,8 +1,166 @@
 # Authors: Miriam Penger, Lena Gerken, Tina Höflich
 # Handles various actions concerning the ball in pong
 
+# TODO: check and adapt these values. important: they are used to check with the center of the ball, not its closest edge.
+# TODO: use display measurements as a constant defined up here
 .eqv LEFT_SCORING_BORDER 40	# assuming a display width of 256 and a scoring area width of 40
 .eqv RIGHT_SCORING_BORDER 216	# assuming a display width of 256 and a scoring area width of 40
+
+# TODO: test this function!
+# TODO: check if an x axis correction of the ball coordinates is necessary with a paddle collision like with the edge collision
+# shares s registers 1-9 with the main function
+# moves and draws the ball, checks for collisions and calls the functions to deal with them
+# needs to be called in a loop by the main function until something happens that needs to be dealt with, e.g. a score
+# frequency of calling this function defines the speed of the game
+ball_control:
+# gets:
+# s1 current x coordinate of the ball
+# s2 current y coordinate of the ball
+# s3 x-component of the ball's current vector
+# s4 y-component of the ball's current vector
+# s5 highest y coordinate reached by the paddle on the left
+# s6 lowest y coordinate reached by the paddle on the left
+# s7 highest y coordinate reached by the paddle on the right
+# s8 lowest y coordinate reached by the paddle on the right
+# returns:
+# s1 new x coordinate of the ball
+# s2 new y coordinate of the ball
+# s3 new (or the same) x-component of the ball's current vector
+# s4 new (or the same) y-component of the ball's current vector
+# s9 score indicator: 0 no score, 1 score on the left side, 2 score on the right side (on, not by)
+
+addi sp, sp, -4
+sw ra, (sp)
+# update ball position
+mv a2, s3
+mv a3, s4
+mv a4, s1
+mv a5, s2
+jal update_ball_position
+mv t1, a0	# t1 new x coordinate of the ball
+mv t2, a1	# t2 new y coordinate of the ball
+
+# check if the border of the display was hit
+li a1, t2
+jal check_ball_edges
+beq a0, zero, no_edge_collision	# a0 = 0 if no collision happened
+li t0, 1
+# correct the ball position in a way that it fits on the display
+beq a0, t0, other_border	# a0 = 1 if the ball has hit the upper border, a0 = 2 if the ball has hit the lower border
+li s2, 125	# lower border collision
+mv a2, s3
+mv a3, s4
+jal change_ball_direction_wall
+mv s3, a0
+mv s4, a1
+j no_edge_collision # done
+other_border:	
+li s2, 3 # upper border collision
+mv a2, s3
+mv a3, s4
+jal change_ball_direction_wall
+mv s3, a0
+mv s4, a1
+
+no_edge_collision: # continue normally, no matter if we just dealt with an edge collision
+# move ball on the display
+mv a1, s1
+mv a2, s2
+mv a3, t1
+mv a4, t2
+jal move_ball
+
+# update the ball's position
+mv s1, t1
+mv s2, t2
+
+# check for collisions
+mv a1, s1
+jal check_ball_position
+beq a0, zero, no_collision	# a0 = 0 if the ball is within the playing area
+li t0, 1
+beq a0, t0, left_collision	# a0=1 if the ball is in the left scoring area
+# a0=2 if the ball is in the right scoring area
+#check_paddle_hit with right paddle data
+mv a1, s2
+mv a2, s7
+mv a3, s8
+jal check_paddle_hit
+beq a0, zero, right_paddle_hit # a0 = 0 if the ball hit the paddle
+# a0 = 1 if the ball didn't hit the paddle
+li s9, 2	#SCORE indicator
+j no_collision	# done
+right_paddle_hit:
+#change_ball_direction_paddle
+mv a2, s3
+mv a3, s4
+mv a4, s1
+mv a5, s2
+mv a6, s7
+mv a7, s8
+jal change_ball_direction_paddle
+# update vector
+mv s3, a0
+mv s4, a1
+# also set the x coordinate of the ball's position as the value of the border that was just hit to avoid a double collision
+li s1, RIGHT_SCORING_BORDER	#TODO: check if this is really necessary and if it works, as the ball's picture is not updated. maybe this is necessary...
+j no_collision	# no collision anymore, as we dealt with it
+
+left_collision:
+#check_paddle_hit with left paddle data
+mv a1, s2
+mv a2, s5
+mv a3, s6
+jal check_paddle_hit
+beq a0, zero, left_paddle_hit	# a0 = 0 if the ball hit the paddle
+# a0 = 1 if the ball didn't hit the paddle
+li s9, 1	#SCORE indicator
+j no_collision	# done
+left_paddle_hit:
+#change_ball_direction_paddle
+mv a2, s3
+mv a3, s4
+mv a4, s1
+mv a5, s2
+mv a6, s5
+mv a7, s6
+jal change_ball_direction_paddle
+# update vector
+mv s3, a0
+mv s4, a1
+# also set the x coordinate of the ball's position as the value of the border that was just hit to avoid a double collision
+li s1, LEFT_SCORING_BORDER	#TODO: check if this is really necessary and if it works, as the ball's picture is not updated. maybe this is necessary...
+#j no_collision	# no collision anymore, as we dealt with it
+
+no_collision:
+lw ra, (sp)
+addi sp, sp, 4
+ret
+
+
+# checks if the ball has hit the edge of the playing area.
+# needs to be called BEFORE the ball is drawn
+check_ball_edges:
+# gets.
+# a1 current y coordinate of the ball
+# returns
+# a0 = 0 if no collision happened
+# a0 = 1 if the ball has hit the upper border
+# a0 = 2 if the ball has hit the lower border
+ble a1, zero, upper_border
+li t0, 128	# lower border
+bge a1, t0, lower_border
+# no collision
+li a0, 0
+j end_check_edges
+lower_border:
+li a0, 2
+j end_check_edges
+upper_border:
+li a0, 1
+end_check_edges:
+ret
+
 
 # function to find out whether the ball reached the scoring area
 check_ball_position:
@@ -78,6 +236,7 @@ mul a1, a3, t0	# y = y*(-1)
 end_switch:
 ret
 
+# TODO: a2 and a3 aren't needed?!
 # function to calculate the new direction of the ball if it hit the edge of the playing field: the closer to the edge of the paddle, the bigger the angle
 change_ball_direction_paddle:
 # gets:
@@ -190,7 +349,7 @@ add a1, a5, a3
 ret
 
 # TODO: see if the vector solution for speed runs smooth enough, add a speed handling function if necessary
-# TODO: some kind of control function to bring it all together
+# TODO: some kind of control function to bring it all together -> it also needs to remember the previous position to delete the old ball
 
 # function to initialize the ball by giving it a random y-Position and direction
 init_ball:
