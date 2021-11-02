1b7072ce82d49dce3d06836d87663e946b529988
lena143 <80892941+lena143@users.noreply.github.com>
2021-11-02 17:40:09 +0100

    bug fixing
    found and fixed a bigger logical mistake and a couple smaller bugs,
    tested the function and it's working fine now

--- src/move_ball.asm
+++ src/move_ball.asm
@@ -2,162 +2,365 @@
 
 # Moves the ball of the pong game using the draw_rectangle function.
 # Only paints the pixel differences of old and new position after movement for more efficient performance.
+# Paints two black rectangles to cover the sections of the ball that aren't needed anymore. only draws the overlapping corner part once, with the horizontal component.
+# Paints two white rectangles to add the new sections to the ball. only draws the overlapping corner part once, with the horizontal component.
+# Since the position of the rectangles depends on the general direction in which the ball is heading, there is a case for all 4 of them
 
 move_ball:
 #----Inputs:----
 # a1: x coordinate of old point
 # a2: y coordinate of old point
 # a3: x coordinate of new point
-# a4: y coordinate of new point
-	
-	#Save all necessary values to the stack
+# a4: y coordinate of new point	
+
+	# Save all necessary values to the stack
 	addi sp, sp, -20
 	sw ra, (sp)
 	sw a1, 4 (sp)
 	sw a2, 8 (sp)
 	sw a3, 12 (sp)
-	sw a4, 16 (sp)
-	
-	sub t0, a3, a1  # x-Sprungweite
-	sub t1, a4, a2  # y-Sprungweite
-	
+	sw a4, 16 (sp)	
 
-	##################### paint old position black #####################
-
-	# calculate first old rectangle
-	ble a1, a3, x_right_old
+	sub t0, a3, a1  # x-jumping-width
+	sub t1, a4, a2  # y-jumping-height
 	
-	# calculate horizontal shift to the left
+	ble a1, a3, x_right
+	ble a2, a4, y_down_1
+	
+	########## up and to the left ##########
+	
+	# black vertical rectangle
 	addi t3, a3, 3		# x1 = x_new + 3
-	add t5, t3, t0		# x2 = x_new + 3 + x-Sprungweite
 	addi t4, a2, -3		# y1 = y_old - 3
+	addi t5, a1, 3		# x2 = x_old + 3
 	addi t6, a2, 3		# y2 = y_old + 3
-	j exit_1
 	
-	# calculate horizontal shift to the right
-	x_right_old:
-	addi t3, a3, -3		# x1 = x_new - 3
-	sub t5, t3, t0		# x2 = x_new - 3 - x-Sprungweite
-	addi t4, a2, -3		# y1 = y_old - 3
-	addi t6, a2, 3		# y2 = y_old + 3
-	j exit_1
-	
-	exit_1:
-	# paint old one black
+	# paint
 	mv a3, t3
 	mv a4, t4
 	mv a5, t5
 	mv a6, t6
 	li a7, 0	# black
-	jal draw_rectangle
-	
+	#li a7, 0x0000ff	# blue FOR TESTING PURPOSES
+	jal draw_rectangle	
 	# Restore values
-	lw ra, 0 (sp)
+	lw a1, 4 (sp)
+	lw a2, 8 (sp)
+	lw a3, 12 (sp)
+	lw a4, 16 (sp)
+
+	# black horizontal rectangle	
+	addi t3, a1, -3		# x1 = x_old - 3 
+	addi t5, a3, 3		# x2 = x_new + 3
+	addi t4, a4, 3		# y1 = y_new + 3
+	addi t6, a2, 3		# y2 = y_old + 3
+	
+	# paint
+	mv a3, t3
+	mv a4, t4
+	mv a5, t5
+	mv a6, t6
+	li a7, 0	# black
+	#li a7, 0x0000ff	# blue FOR TESTING PURPOSES
+	jal draw_rectangle	
+	# Restore values
 	lw a1, 4 (sp)
 	lw a2, 8 (sp)
 	lw a3, 12 (sp)
 	lw a4, 16 (sp)
 	
-	ble a2, a4, y_down_old	# if (y_old <= y_new)
-	
-	# calculate vertical shift up
-	addi t3, a1, -3		# x1 = x_old - 3
-	addi t5, a3, 3		# x2 = x_new + 3
-	addi t4, a4, 3		# y1 = y_new + 3
-	addi t6, a2, 3		# y2 = y_old + 3
-	j exit_2
-	
-	# calculate vertical shift down
-	y_down_old:
-	addi t3, a3, -3		# x1 = x_new - 3
-	addi t5, a1, 3		# x2 = x_old + 3
-	addi t4, a2, -3		# y1 = y_old - 3
-	addi t6, a4, -3		# y2 = y_new - 3
-	j exit_2
-	
-	exit_2:
-	# paint old one black
-	mv a3, t3
-	mv a4, t4
-	mv a5, t5
-	mv a6, t6
-	li a7, 0	# black
-	jal draw_rectangle
-	
-
-	##################### paint new position white #####################
-
-	# calculate first new rectangle
-	ble a1, a3, x_right_new
-	
-	# calculate horizontal shift to the left
+	# white vertical rectangle
 	addi t3, a3, -3		# x1 = x_new - 3
 	addi t5, a1, -3		# x2 = x_old - 3
 	addi t4, a4, -3		# y1 = y_new - 3
 	addi t6, a4, 3		# y2 = y_new + 3
-	j exit_3
 	
-	# calculate horizontal shift to the right
-	x_right_new:
+	# paint white
+	mv a3, t3
+	mv a4, t4
+	mv a5, t5
+	mv a6, t6
+	li a7, 0xffffff
+	jal draw_rectangle
+	# Restore values
+	lw a1, 4 (sp)
+	lw a2, 8 (sp)
+	lw a3, 12 (sp)
+	lw a4, 16 (sp)
+	
+	# white horizontal rectangle
+	addi t3, a3, -3 	# x1 = x_new - 3
+	addi t5, a3, 3		# x2 = x_new + 3.
+	addi t4, a4, -3		# y1 = y_new - 3
+	addi t6, a2, -3		# y2 = y_old - 3
+	
+	# paint white
+	mv a3, t3
+	mv a4, t4
+	mv a5, t5
+	mv a6, t6
+	li a7, 0xffffff
+	jal draw_rectangle
+	# Restore values
+	lw a1, 4 (sp)
+	lw a2, 8 (sp)
+	lw a3, 12 (sp)
+	lw a4, 16 (sp)
+	
+	j end_case
+	
+	########## down and to the left ##########
+	
+	y_down_1:
+			
+	# black vertical rectangle
+	addi t3, a3, 3		# x1 = x_new + 3
+	addi t4, a2, -3		# y1 = y_old - 3
+	addi t5, a1, 3		# x2 = x_old + 3
+	addi t6, a2, 3		# y2 = y_old + 3
+	
+	# paint
+	mv a3, t3
+	mv a4, t4
+	mv a5, t5
+	mv a6, t6
+	li a7, 0	# black
+	#li a7, 0x0000ff	# blue FOR TESTING PURPOSES
+	jal draw_rectangle	
+	# Restore values
+	lw a1, 4 (sp)
+	lw a2, 8 (sp)
+	lw a3, 12 (sp)
+	lw a4, 16 (sp)
+
+	# black horizontal rectangle	
+	addi t3, a1, -3		# x1 = x_old - 3 
+	addi t5, a3, 3		# x2 = x_new + 3
+	addi t4, a2, -3		# y1 = y_old - 3
+	addi t6, a4, -3		# y2 = y_new - 3
+	
+	# paint
+	mv a3, t3
+	mv a4, t4
+	mv a5, t5
+	mv a6, t6
+	li a7, 0	# black
+	#li a7, 0x0000ff	# blue FOR TESTING PURPOSES
+	jal draw_rectangle	
+	# Restore values
+	lw a1, 4 (sp)
+	lw a2, 8 (sp)
+	lw a3, 12 (sp)
+	lw a4, 16 (sp)
+	
+	# white vertical rectangle
+	addi t3, a3, -3		# x1 = x_new - 3
+	addi t5, a1, -3		# x2 = x_old - 3
+	addi t4, a4, -3		# y1 = y_new - 3
+	addi t6, a4, 3		# y2 = y_new + 3
+	
+	# paint white
+	mv a3, t3
+	mv a4, t4
+	mv a5, t5
+	mv a6, t6
+	li a7, 0xffffff
+	jal draw_rectangle
+	# Restore values
+	lw a1, 4 (sp)
+	lw a2, 8 (sp)
+	lw a3, 12 (sp)
+	lw a4, 16 (sp)
+	
+	# white horizontal rectangle
+	addi t3, a1, -3 	# x1 = x_old - 3
+	addi t5, a3, 3		# x2 = x_new + 3.
+	addi t4, a2, 3		# y1 = y_old + 3
+	addi t6, a4, 3		# y2 = y_new + 3
+	
+	# paint white
+	mv a3, t3
+	mv a4, t4
+	mv a5, t5
+	mv a6, t6
+	li a7, 0xffffff
+	jal draw_rectangle
+	# Restore values
+	lw a1, 4 (sp)
+	lw a2, 8 (sp)
+	lw a3, 12 (sp)
+	lw a4, 16 (sp)
+	
+	j end_case
+	x_right:
+	ble a2, a4, y_down_2
+	
+	########## up and to the right ##########
+	
+	# black vertical rectangle
+	addi t3, a1, -3		# x1 = x_old - 3
+	addi t5, a3, -3 	# x2 = x_new - 3
+	addi t4, a2, -3		# y1 = y_old - 3
+	addi t6, a2, 3		# y2 = y_old + 3
+	
+	# paint
+	mv a3, t3
+	mv a4, t4
+	mv a5, t5
+	mv a6, t6
+	li a7, 0	# black
+	#li a7, 0x0000ff	# blue FOR TESTING PURPOSES
+	jal draw_rectangle	
+	# Restore values
+	lw a1, 4 (sp)
+	lw a2, 8 (sp)
+	lw a3, 12 (sp)
+	lw a4, 16 (sp)
+
+	# black horizontal rectangle	
+	addi t3, a3, -3		# x1 = x_new - 3 
+	addi t5, a1, 3		# x2 = x_old + 3
+	addi t4, a4, 3		# y1 = y_new + 3
+	addi t6, a2, 3		# y2 = y_old + 3
+	
+	# paint
+	mv a3, t3
+	mv a4, t4
+	mv a5, t5
+	mv a6, t6
+	li a7, 0	# black
+	#li a7, 0x0000ff	# blue FOR TESTING PURPOSES
+	jal draw_rectangle	
+	# Restore values
+	lw a1, 4 (sp)
+	lw a2, 8 (sp)
+	lw a3, 12 (sp)
+	lw a4, 16 (sp)
+	
+	# white vertical rectangle
 	addi t3, a1, 3		# x1 = x_old + 3
 	addi t5, a3, 3		# x2 = x_new + 3
 	addi t4, a4, -3		# y1 = y_new - 3
 	addi t6, a4, 3		# y2 = y_new + 3
-	j exit_3
 	
-	exit_3:
-	# paint new one white
+	# paint white
 	mv a3, t3
 	mv a4, t4
 	mv a5, t5
 	mv a6, t6
 	li a7, 0xffffff
 	jal draw_rectangle
-	
 	# Restore values
-	lw ra, 0 (sp)
 	lw a1, 4 (sp)
 	lw a2, 8 (sp)
 	lw a3, 12 (sp)
 	lw a4, 16 (sp)
 	
-	ble a2, a4, y_down_new	# if (y_old <= y_new)
-	
-	# calculate vertical shift up
-	addi t3, a1, -3		# x1 = x_old - 3
-	addi t5, a3, 3		# x2 = x_new + 3
+	# white horizontal rectangle
+	addi t3, a3, -3 	# x1 = x_new - 3
+	addi t5, a1, 3		# x2 = x_old + 3.
 	addi t4, a4, -3		# y1 = y_new - 3
 	addi t6, a2, -3		# y2 = y_old - 3
-	j exit_4
 	
-	# calculate vertical shift down
-	y_down_new:
-	addi t3, a3, -3		# x1 = x_new - 3
-	addi t5, a1, 3		# x2 = x_old + 3
-	addi t4, a2, 3		# y1 = y_old + 3
-	addi t6, a4, 3		# y2 = y_new + 3
-	j exit_4
-	
-	exit_4:
-	# paint new one white
+	# paint white
 	mv a3, t3
 	mv a4, t4
 	mv a5, t5
 	mv a6, t6
 	li a7, 0xffffff
 	jal draw_rectangle
-
-	
-	# Restore and jump back
-	lw ra, 0 (sp)
+	# Restore values
 	lw a1, 4 (sp)
 	lw a2, 8 (sp)
 	lw a3, 12 (sp)
 	lw a4, 16 (sp)
-	addi sp, sp, 20
+	
+	j end_case
+	
+	########## down and to the right ##########
+	
+	y_down_2:
+		
+	# black vertical rectangle
+	addi t3, a1, -3		# x1 = x_old - 3
+	addi t5, a3, -3 	# x2 = x_new - 3
+	addi t4, a2, -3		# y1 = y_old - 3
+	addi t6, a2, 3		# y2 = y_old + 3
+	
+	# paint
+	mv a3, t3
+	mv a4, t4
+	mv a5, t5
+	mv a6, t6
+	li a7, 0	# black
+	#li a7, 0x0000ff	# blue FOR TESTING PURPOSES
+	jal draw_rectangle	
+	# Restore values
+	lw a1, 4 (sp)
+	lw a2, 8 (sp)
+	lw a3, 12 (sp)
+	lw a4, 16 (sp)
+
+	# black horizontal rectangle	
+	addi t3, a3, -3		# x1 = x_new - 3 
+	addi t5, a1, 3		# x2 = x_old + 3
+	addi t4, a2, -3		# y1 = y_old - 3
+	addi t6, a4, -3		# y2 = y_new - 3
+	
+	# paint
+	mv a3, t3
+	mv a4, t4
+	mv a5, t5
+	mv a6, t6
+	li a7, 0	# black
+	#li a7, 0x0000ff	# blue FOR TESTING PURPOSES
+	jal draw_rectangle	
+	# Restore values
+	lw a1, 4 (sp)
+	lw a2, 8 (sp)
+	lw a3, 12 (sp)
+	lw a4, 16 (sp)
+	
+	# white vertical rectangle
+	addi t3, a1, 3		# x1 = x_old + 3
+	addi t5, a3, 3		# x2 = x_new + 3
+	addi t4, a4, -3		# y1 = y_new - 3
+	addi t6, a4, 3		# y2 = y_new + 3
+	
+	# paint white
+	mv a3, t3
+	mv a4, t4
+	mv a5, t5
+	mv a6, t6
+	li a7, 0xffffff
+	jal draw_rectangle
+	# Restore values
+	lw a1, 4 (sp)
+	lw a2, 8 (sp)
+	lw a3, 12 (sp)
+	lw a4, 16 (sp)
+	
+	# white horizontal rectangle
+	addi t3, a3, -3 	# x1 = x_new - 3
+	addi t5, a1, 3		# x2 = x_old + 3.
+	addi t4, a2, 3		# y1 = y_old + 3
+	addi t6, a4, 3		# y2 = y_new + 3
+	
+	# paint white
+	mv a3, t3
+	mv a4, t4
+	mv a5, t5
+	mv a6, t6
+	li a7, 0xffffff
+	jal draw_rectangle
+	# Restore values
+	lw a1, 4 (sp)
+	lw a2, 8 (sp)
+	lw a3, 12 (sp)
+	lw a4, 16 (sp)
+	
+	end_case:
+	lw ra, 0 (sp)
 	ret
-
-li a7, 10
-ecall
-
-.include "draw_rectangle.asm"
+	
+	.include "draw_rectangle.asm"
