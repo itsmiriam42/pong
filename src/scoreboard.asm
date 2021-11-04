67ca4888ef34e81d7b03cca69dbb58945868df8b
Tina <44570841+tina-hoeflich@users.noreply.github.com>
2021-11-04 13:37:52 +0100

    remove main function

--- src/scoreboard.asm
+++ src/scoreboard.asm
@@ -1,19 +1,17 @@
-# Authors: Miriam Penger, Lena Gerken, Tina Höflich
+# Authors: Miriam Penger, Lena Gerken, Tina HÃ¶flich
 
-#draws the scoreboard of the pong game pixel by pixel using the draw pixel function
-#
-
-j main
-
-.include "draw_pixel.asm"
-.include "draw_line.asm"
-.include "draw_circle.asm"
+# function draws the scoreboard of the pong game pixel by pixel using the draw pixel function
+# gets:
+# a2: the decimal number between 0 and 11 that then are displayed on the bitmap display
+# returns:
+# - 
 
 .text
 
-#Funktion to draw dots that separate the numbers on the scoreboard
+# Funktion to draw dots that separate the numbers on the scoreboard
+# drawing two circles using "draw_circle.asm"
 draw_dots:
-#allocate Memory:
+# allocate Memory:
 addi sp, sp,-20
 sw a3,   (sp)
 sw a4,  4 (sp)
@@ -21,24 +19,26 @@ sw a5, 8 (sp)
 sw a7, 12 (sp)
 sw ra,  16 (sp)
 
-#draw dots
+# draw dots
   li a3, 117
   li a4, 14
   li a5, 1
-  li a7, 0xffffff #white
+  li a7, 0xffffff # white
   jal draw_circle
   li a3, 117
   li a4, 20
   li a5, 1
-  li a7, 0xffffff #white
+  li a7, 0xffffff # white
   jal draw_circle
   
-  #restore and Jump back
+  # restore and Jump back
  lw ra, 16 (sp)
  addi sp, sp, 20
  ret
  
- #Function to draw left numbers on the scoreboard
+ # Function to draw left numbers on the scoreboard
+ # gets:
+ # a2: number that is displayed on the screen 
 draw_left_number:
 # allocate memory for x0-y1 + color variable
 addi sp, sp,-28
@@ -50,16 +50,16 @@ sw a6, 16 (sp)
 sw a7, 20 (sp)
 sw ra,  24 (sp)
 
-#draw a black 18 to remove numbers:
+# draw a black 18 to remove numbers:
 # ----------- 18-----------
-  #-1---
+  # -1---
   li a3, 95
   li a4, 10
   li a5, 95
   li a6, 25
   li a7,0
   jal draw_line
-    #-1----
+    # -1----
   li a3, 110
   li a4, 10
   li a5, 110
@@ -92,8 +92,8 @@ sw ra,  24 (sp)
   jal draw_line
 
 
-li a7, 0xffffff #white
-lw t0, (sp) #input number
+li a7, 0xffffff # white
+lw t0, (sp) # input number
 switch.start:
 # ----------- 0 ------------
   switch.0:
@@ -120,7 +120,7 @@ switch.start:
   li a6, 25
   jal draw_line
   beq zero, zero switch.end
- #----------------1------------
+ # ----------------1------------
    switch.1:
    li t1, 1
   bne t0, t1 switch.2
@@ -130,7 +130,7 @@ switch.start:
   li a6, 25  
   jal draw_line
    beq zero, zero switch.end
- #---------------2-------------
+ # ---------------2-------------
   switch.2:
    li t1, 2
   bne t0, t1 switch.3
@@ -159,7 +159,7 @@ switch.start:
   li a5, 110
   li a6, 17
   jal draw_line
-  #-------------3---------------
+  # -------------3---------------
   switch.3:
    li t1, 3
   bne t0, t1 switch.4
@@ -189,7 +189,7 @@ switch.start:
   li a6, 17
   jal draw_line
   beq zero, zero switch.end
-  #--------------4-------------
+  # --------------4-------------
    switch.4:
    li t1, 4
   bne t0, t1 switch.5
@@ -209,7 +209,7 @@ switch.start:
   li a6, 17
   jal draw_line
   beq zero, zero switch.end
-  #--------------5------------
+  # --------------5------------
   switch.5:
    li t1, 5
   bne t0, t1 switch.6
@@ -239,7 +239,7 @@ switch.start:
   li a6, 25
   jal draw_line
   beq zero, zero switch.end
-   #--------------6------------
+   # --------------6------------
   switch.6:
    li t1, 6
   bne t0, t1 switch.7
@@ -269,7 +269,7 @@ switch.start:
   li a6, 10
   jal draw_line
   beq zero, zero switch.end
-  #--------------7------------
+  # --------------7------------
   switch.7:
    li t1,7
   bne t0, t1 switch.8
@@ -348,7 +348,7 @@ switch.start:
   switch.10:
   li t1,10
   bne t0, t1 switch.11
-  #-0----
+  # -0----
   li a3, 100
   li a4, 10
   li a5, 100
@@ -369,7 +369,7 @@ switch.start:
   li a5, 110
   li a6, 25
   jal draw_line
-  #-1---
+  # -1---
   li a3, 95
   li a4, 10
   li a5, 95
@@ -380,13 +380,13 @@ switch.start:
   switch.11:
   li t1,11
   bne t0, t1 switch.end
-  #-1----
+  # -1----
   li a3, 110
   li a4, 10
   li a5, 110
   li a6, 25  
   jal draw_line
-  #-1---
+  # -1---
   li a3, 95
   li a4, 10
   li a5, 95
@@ -398,7 +398,7 @@ switch.start:
  addi sp, sp, 28
  ret
 
-#function to draw right number to the scoreboard
+# function to draw right number to the scoreboard
 draw_right_number:
 # allocate memory for x0-y1 + color variable
 addi sp, sp,-28
@@ -410,18 +410,18 @@ sw a6, 16 (sp)
 sw a7, 20 (sp)
 sw ra, 24 (sp)
 
-#-----------dots---------
+# -----------dots---------
   
-#draw a black 18 to remove numbers:
+# draw a black 18 to remove numbers:
 # ----------- 18-----------
-  #-1---
+  # -1---
   li a3, 125
   li a4, 10
   li a5, 125
   li a6, 25
   li a7,0
   jal draw_line
-    #-1----
+    # -1----
   li a3, 140
   li a4, 10
   li a5, 140
@@ -454,8 +454,8 @@ sw ra, 24 (sp)
   jal draw_line
 
 
-  li a7, 0xffffff #white
-  lw t0, (sp) #input number
+  li a7, 0xffffff # white
+  lw t0, (sp) # input number
   switch.start_2:
  # ----------- 0 ------------
   switch.null:
@@ -482,7 +482,7 @@ sw ra, 24 (sp)
   li a6, 25
   jal draw_line
   beq zero, zero switch.end_2
- #----------------1------------
+ # ----------------1------------
    switch.eins:
    li t1, 1
   bne t0, t1 switch.zwei
@@ -492,7 +492,7 @@ sw ra, 24 (sp)
   li a6, 25  
   jal draw_line
    beq zero, zero switch.end_2
- #---------------2-------------
+ # ---------------2-------------
   switch.zwei:
    li t1, 2
   bne t0, t1 switch.drei
@@ -521,7 +521,7 @@ sw ra, 24 (sp)
   li a5, 140
   li a6, 17
   jal draw_line
-  #-------------3---------------
+  # -------------3---------------
   switch.drei:
    li t1, 3
   bne t0, t1 switch.vier
@@ -551,7 +551,7 @@ sw ra, 24 (sp)
   li a6, 17
   jal draw_line
   beq zero, zero switch.end_2
-  #--------------4-------------
+  # --------------4-------------
    switch.vier:
    li t1, 4
   bne t0, t1 switch.fuenf
@@ -571,7 +571,7 @@ sw ra, 24 (sp)
   li a6, 17
   jal draw_line
   beq zero, zero switch.end_2
-  #--------------5------------
+  # --------------5------------
   switch.fuenf:
    li t1, 5
   bne t0, t1 switch.sechs
@@ -601,7 +601,7 @@ sw ra, 24 (sp)
   li a6, 25
   jal draw_line
   beq zero, zero switch.end_2
-   #--------------6------------
+   # --------------6------------
   switch.sechs:
    li t1, 6
   bne t0, t1 switch.sieben
@@ -631,7 +631,7 @@ sw ra, 24 (sp)
   li a6, 10
   jal draw_line
   beq zero, zero switch.end_2
-  #--------------7------------
+  # --------------7------------
   switch.sieben:
    li t1,7
   bne t0, t1 switch.acht
@@ -710,7 +710,7 @@ sw ra, 24 (sp)
   switch.zehn:
   li t1,10
   bne t0, t1 switch.elf
-  #-0----
+  # -0----
   li a3, 130
   li a4, 10
   li a5, 130
@@ -731,7 +731,7 @@ sw ra, 24 (sp)
   li a5, 140
   li a6, 25
   jal draw_line
-  #-1---
+  # -1---
   li a3, 125
   li a4, 10
   li a5, 125
@@ -742,13 +742,13 @@ sw ra, 24 (sp)
   switch.elf:
   li t1,11
   bne t0, t1 switch.end_2
-  #-1----
+  # -1----
   li a3, 140
   li a4, 10
   li a5, 140
   li a6, 25  
   jal draw_line
-  #-1---
+  # -1---
   li a3, 125
   li a4, 10
   li a5, 125
@@ -761,19 +761,10 @@ sw ra, 24 (sp)
  addi sp, sp, 28
  ret
  
- #Main function to test, will be called by other functions!! 
-main:
+#function to initialize the scoreboard with 0 : 0 
+init_scoreboard:
 jal draw_dots
-li a2,11
+li a2,0
 jal draw_left_number
-
-li a2,11
+li a2,0
 jal draw_right_number
-
-
-li a2,1
-jal draw_left_number
-
-
-li a2,10
-jal draw_left_number
