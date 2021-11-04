3d075e74fb7566ec3c2b758e5c87e7bf384389f7
Tina <44570841+tina-hoeflich@users.noreply.github.com>
2021-11-04 20:05:47 +0100

    update cesplib_rars

--- src/cesplib_rars.asm
+++ src/cesplib_rars.asm
@@ -4,10 +4,6 @@
 .eqv DISPLAY_HEIGHT 128
 .eqv KEYBOARD_ADDDRESS 0xFFFF0000
 
-bmp_filename_winner_r: .string "C:/rars_programming_project/pong-main/src/winner_right.bmp"
-bmp_filename_start: .string "C:/rars_programming_project/pong-main/src/startbildschirm.bmp"
-mp_filename_winner_l: .string "C:/rars_programming_project/pong-main/src/winner_right.bmp"
-
 #cesp_sleep:
 # Input:
 #   a0: number of ms to sleep
