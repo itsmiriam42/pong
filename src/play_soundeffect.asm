bbd78a5f09187bd65ffee51840eb552bd7e67421
Tina <44570841+tina-hoeflich@users.noreply.github.com>
2021-11-04 14:10:21 +0100

    change soundeffect

--- src/play_soundeffect.asm
+++ src/play_soundeffect.asm
@@ -1,26 +1,23 @@
 
 #function to play a short soundeffect 
+# gets:
+# - 
+# returns:
+# - 
 .text
 
-main:                                   
-        
-	jal play_soundeffect  
-	#end
-	li a7, 10
-	ecall
-	
  
 play_soundeffect:
 
 	addi sp, sp, -4
 	sw ra, 0x0 (sp)
 	li a7,33
-        addi    a0, zero, 60
-       	addi 	a1, zero, 400
+        addi    a0, zero, 45
+       	addi 	a1, zero, 500
         addi    a3, zero, 127
-        addi    a2, zero, 113 #bass
+        addi    a2, zero, 118 #Percussion
         ecall   
-
+       
         #restore
         lw ra, 0 (sp)
 	addi sp, sp, 4
