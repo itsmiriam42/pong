2373568149a1b2ca53ae2f8ab19ce5a41b58898d
Miriam <itsmiriam@posteo.de>
2021-11-05 20:05:27 +0100

    updated paddle movement speed from 3 to 5

--- src/cesplib_rars.asm
+++ src/cesplib_rars.asm
@@ -3,14 +3,14 @@
 .eqv DISPLAY_WIDTH 256
 .eqv DISPLAY_HEIGHT 128
 .eqv KEYBOARD_ADDDRESS 0xFFFF0000
-.eqv LEFT_SCORING_BORDER 26		# left scoring border is reached when the ball's center reaches this x coordinate value
+.eqv LEFT_SCORING_BORDER 26	# left scoring border is reached when the ball's center reaches this x coordinate value
 .eqv RIGHT_SCORING_BORDER 230	# right scoring border is reached when the ball's center reaches this x coordinate value
 .eqv PADDLE_WIDTH 5
 .eqv PADDLE_HEIGHT 30
-.eqv PADDLE_MOVEMENT_SPEED 3	# number of pixels that the paddle moves per keystroke
+.eqv PADDLE_MOVEMENT_SPEED 5	# number of pixels that the paddle moves per keystroke
 .eqv LEFT_PADDLE_BOUNDARY 18	# initial x-coordinate of the left side of the left paddle
 .eqv RIGHT_PADDLE_BOUNDARY 233	# initial x-coordinate of the left side of the right paddle
-.eqv TOP_PADDLE_BOUNDARY 50		# initial y-coordinate of the top side of both paddles
+.eqv TOP_PADDLE_BOUNDARY 50	# initial y-coordinate of the top side of both paddles
 
 #cesp_sleep:
 # Input:
