4ce6fe92be479e21774e7a1921f82c126713c7a5
Miriam <itsmiriam@posteo.de>
2021-11-05 15:54:25 +0100

    exchanged hardcoded values for variable ones

--- src/draw_paddles.asm
+++ src/draw_paddles.asm
@@ -1,11 +1,12 @@
 # Authors: Miriam Penger, Lena Gerken, Tina Hoeflich
 
 # Draws both paddles using the draw_rectangle function.
-# Note: This only acts as an init function on startup.
+# This only acts as an init function on startup.
+# Looped movement via keyboard is done in move_paddles.asm
 
-# returns:
-# a4: highest y coordinate reached by the paddle on the left
-# a6: highest y coordinate reached by the paddle on the right
+# Returns:
+# a4: highest y-coordinate reached by the paddle on the left
+# a6: highest y-coordinate reached by the paddle on the right
 
 draw_paddles:
 	
@@ -15,24 +16,37 @@ draw_paddles:
 	
 	# initial setup of rectangle coordinates
 	# left paddle
-	li a3, 18	# rectangle x0
-	li a4, 50	# rectangle y0
-	li a5, 23	# rectangle x1
-	li a6, 80	# rectangle y1
-	li a7, 0xffffff	# color: white
+	li t0, LEFT_PADDLE_BOUNDARY			
+	mv a3, t0		# rectangle x0 = 18
+	li t3, TOP_PADDLE_BOUNDARY
+	mv a4, t3		# rectangle y0 = 50
+	li t1, PADDLE_WIDTH
+	add t2, t0, t1
+	mv a5, t2		# rectangle x1 = 23 (18 + 5)
+	li t2, PADDLE_HEIGHT
+	add t3, a4, t2
+	mv a6, t3		# rectangle y1 = 80 (50 + 30)
+	li a7, 0xffffff		# color: white
 	jal draw_rectangle
 	
 	# right paddle
-	li a3, 233	# rectangle x0
-	li a4, 50	# rectangle y0
-	li a5, 238	# rectangle x1
-	li a6, 80	# rectangle y1
-	li a7, 0xffffff	# color: white
+	li t0, RIGHT_PADDLE_BOUNDARY
+	mv a3, t0		# rectangle x0 = 233
+	li t3, TOP_PADDLE_BOUNDARY
+	mv a4, t3		# rectangle y0 = 50
+	li t1, PADDLE_WIDTH
+	add t2, t0, t1
+	mv a5, t2		# rectangle x1 = 238 (233 + 5)
+	li t2, PADDLE_HEIGHT
+	add t3, a4, t2
+	mv a6, t3		# rectangle y1 = 80 (50 + 30)
+	li a7, 0xffffff		# color: white
 	jal draw_rectangle
 		
-	# Restore and jump back
-	li a4, 50	# returns highest y coordinate reached by the paddle on the left
-	li a6, 50	# returns highest y coordinate reached by the paddle on the right
+	# restore and jump back
+	li t3, TOP_PADDLE_BOUNDARY
+	mv a4, t3	# returns highest y-coordinate reached by the paddle on the left
+	mv a6, t3	# returns highest y-coordinate reached by the paddle on the right
 	lw ra, 0 (sp)
 	addi sp, sp, 4
 	ret
@@ -40,5 +54,3 @@ draw_paddles:
 
 li a7, 10
 ecall
-
-#.include "draw_rectangle.asm"
