1c01813cc97e90a479789a639e451f9d637f1286
lena143 <80892941+lena143@users.noreply.github.com>
2021-11-05 14:28:05 +0100

    added scoring border constants

--- src/cesplib_rars.asm
+++ src/cesplib_rars.asm
@@ -3,6 +3,8 @@
 .eqv DISPLAY_WIDTH 256
 .eqv DISPLAY_HEIGHT 128
 .eqv KEYBOARD_ADDDRESS 0xFFFF0000
+.eqv LEFT_SCORING_BORDER 26	# left scoring border is reached when the ball's center reaches this x coordinate value
+.eqv RIGHT_SCORING_BORDER 240	# right scoring border is reached when the ball's center reaches this x coordinate value
 
 #cesp_sleep:
 # Input:
@@ -10,6 +12,5 @@
   #li a7, 32
   #ecall
   #ret
-
 
 #cesplib.end:
