87ffc8a6098ea74c7aa6b4bed2e52d0c9f37954d
Miriam <itsmiriam@posteo.de>
2021-11-05 23:53:27 +0100

    added draw_ball unittest

---
+++ src/test_draw_ball.asm
@@ -0,0 +1,22 @@
+# Authors: Miriam Penger, Lena Gerken, Tina Höflich
+# Test for the function in draw_ball.asm
+
+# The ball is shown in the upper left corner of the display so that the outer edges are exactly at the edge of the display.
+# When executed correctly, the edge lengths of the ball are equal.
+
+.include "cesplib_rars.asm"
+
+.text
+
+li a1, 3 
+li a2, 3
+li a3, 0xffffff	#white
+
+jal draw_ball
+
+li a7, 10
+ecall
+
+.include "draw_ball.asm"
+.include "draw_rectangle.asm"
+.include "draw_pixel.asm"
