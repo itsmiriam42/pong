5f06d35750ed604d7b6dc9d4f182e14d7ec0129f
tina-hoeflich <44570841+tina-hoeflich@users.noreply.github.com>
2021-11-05 16:10:33 +0100

    Update main.asm

--- src/main.asm
+++ src/main.asm
@@ -82,12 +82,12 @@ bne 	t0, 	t1, 	start_loop
 		beq s9, t1, score_left
 		li t1, 2
 		beq s9, t1, score_right
-		score_left:
+		score_left: # Right Player scored on the left side
 			li t1, 1
 			add s10, t1 ,s10#score left ++
 			li s9, 0 #score indicator = 0
 			mv a2, s10
-			jal draw_left_number
+			jal draw_right_number
 			jal play_soundeffect
 			jal init_ball
 			# move returned values into registers s1-s4 
@@ -102,12 +102,12 @@ bne 	t0, 	t1, 	start_loop
 			mv a2,s2
 			li a3, 0xffffff	#white
 			jal draw_ball
-		score_right:
+		score_right: # Left Player scored on the right side
 			li t1, 1
 			add s11, s11,t1 #score right ++
 			li s9, 0 #score indicator = 0
 			mv a2, s11
-			jal draw_right_number
+			jal draw_left_number
 			jal play_soundeffect
 			jal init_ball
 			# move returned values into registers s1-s4 
@@ -124,13 +124,26 @@ bne 	t0, 	t1, 	start_loop
 			jal draw_ball
 		j while_loop
 	end_game:
+	#print to console
+	li  a7, 1          # Prints 1
+	li a0, 1
+	ecall
 		li t1, 11
-		beq s10, t1 , win_left_player
-		beq s11, t1 , win_right_player
-		win_left_player:
-			jal win_image_left
+		beq s10, t1  win_left_player
+		beq s11, t1  win_right_player
 		win_right_player:
 			jal win_image_right
+			beq zero, zero, sound_end
+			li  a7, 1          # Prints 2
+			li a0, 2
+			ecall
+		win_left_player:
+			jal win_image_left
+			li  a7, 1          # Prints 3
+			li a0, 3
+			ecall
+		
+		sound_end:
 		jal play_sound_brass
 
 	
@@ -147,7 +160,7 @@ bne 	t0, 	t1, 	start_loop
 .include "draw_paddles.asm"
 .include "draw_rectangle.asm"
 .include "move_ball.asm"
-.include "play_sound_win.asm"
+.include "play_sound_win.asm"	
 .include "play_soundeffect.asm"
 .include "scoreboard.asm"
 .include "readwordunaligned.asm"
